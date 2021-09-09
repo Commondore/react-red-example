2162f2a12a588b537f2298b3cb5b145261c2af9b
Alex Som <alex.som86@gmail.com>
2021-09-09 19:54:19 +0600

    add list

--- src/App.js
+++ src/App.js
@@ -6,48 +6,75 @@ function App() {
   const [users, setUsers] = useState([
     { title: "Ketty", age: 30, hobby: "Chess" },
     { title: "Sam", age: 20, hobby: "Football" },
+    { title: "John", age: 40, hobby: "Basketball" },
   ]);
 
-  const changeName = () => {
-    setUsers((prevUsers) => {
-      const users = [...prevUsers];
-      const person = {...prevUsers[0]};
-      person.title = 'Peter';
-      users[0] = person;
+  const changeName = (event, index) => {
+    const person = { ...users[index] };
+    const usersCopy = [...users];
+    person[event.target.name] = event.target.value;
+    usersCopy[index] = person;
 
-      return users;
-    })
+    setUsers(usersCopy);
+    // setUsers((users) => {
+    //   return [
+    //     ...users,
+    //     {
+    //       ...users[index]
+    //     }
+    //   ]
+    // })
+
   };
 
-  const increaseAge = () => {
-    setUsers((prevUsers) => {
-      return prevUsers.map(user => {
-        return {
-          title: user.title,
-          hobby: user.hobby,
-          age: user.age + 1,
-        };
-      })
-    })
+  const increaseAge = (index) => {
+    const person = {...users[index]};
+    person.age++;
+    const usersCopy = [...users];
+    usersCopy[index] = person;
+
+    setUsers(usersCopy);
+
   }
+  // let userList = [];
+  // for (let index = 0; index < users.length; index++) {
+  //   let user = users[index];
+  //   userList.push(
+  //     <Person
+  //       title={user.title}
+  //       age={user.age}
+  //       click={() => increaseAge(index)}
+  //       change={(event) => changeName(event, index)}
+  //     >
+  //       {user.hobby}
+  //     </Person>
+  //   );
+  // }
 
   return (
     <div className="App">
       <div>
-        <button className="btn" onClick={changeName}>
-          Change name
-        </button>
         <button className="btn" onClick={increaseAge}>
           Increase age
         </button>
       </div>
       <div className="people">
-        <Person title={users[0].title} age={users[0].age}>
-          {users[0].hobby}
-        </Person>
-        <Person title={users[1].title} age={users[1].age}>
-          {users[1].hobby}
-        </Person>
+        {
+          // [<Person/>, <Person/>, <Person/>]
+          users.map( (user, index) => {
+            return (
+              <Person
+                key={index}
+                title={user.title}
+                age={user.age}
+                click={() => increaseAge(index)}
+                change={(event) => changeName(event, index)}
+              >
+                {user.hobby}
+              </Person>
+            );
+          })
+        }
       </div>
     </div>
   );
--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,11 +3,24 @@ import './Person.css';
 function Person(props) {
   return (
     <div className="person">
-      <h1 className="title">{props.title}</h1>
-      <p className="age">Age: <span>{props.age}</span></p>
+      <h1 className="title" onClick={props.click}>
+        {props.title}
+      </h1>
+      <p className="age">
+        Age: <span>{props.age}</span>
+      </p>
       <p className="hobby">hobby: {props.children}</p>
+      <div>
+        <input
+          type="text"
+          className="change__name"
+          name="title"
+          value={props.title}
+          onChange={props.change}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Person;
